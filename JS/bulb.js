45ca1ed2add33653d8e6cfa7adfe3b3652d481ed
pranav-gautam <64377125+pranav-gautam@users.noreply.github.com>
2020-10-29 11:27:54 +0530

    Add files via upload

---
+++ JS/bulb.js
@@ -0,0 +1,21 @@
+function light(sw){
+	var pic;
+	if (sw==0){
+		pic="G:/HTML5/Images/pic_bulbon.gif";
+	}
+	else{
+		pic="G:/HTML5/Images/pic_bulboff.gif";
+	}
+	document.getElementById('img1').src=pic;
+}
+
+$("document").ready(function(){
+	$("#btn1").click(function(){
+		$("#p1").text("Aye! band ker isko.");
+		$("#p1").css({"width":"128px"});
+	});
+	$("#btn2").click(function(){
+		$("#p1").text("Light on ker, bhot andhera hai!");
+		$("#p1").css({"width":"200px"});
+	});
+});
