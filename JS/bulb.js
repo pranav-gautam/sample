96a7645b9527d609dfa58fded4e98d5a4cc315c0
pranav-gautam <64377125+pranav-gautam@users.noreply.github.com>
2020-10-30 09:31:25 +0530

    Update bulb.js

--- JS/bulb.js
+++ JS/bulb.js
@@ -1,10 +1,10 @@
 function light(sw){
 	var pic;
 	if (sw==0){
-		pic="sample/Images/pic_bulbon.gif";
+		pic="pic_bulbon.gif";
 	}
 	else{
-		pic="sample/Images/pic_bulboff.gif";
+		pic="pic_bulboff.gif";
 	}
 	document.getElementById('img1').src=pic;
 }
