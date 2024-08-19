eb102dcc76b7e0c72637ec67387b6ad17fe4c347
Pranav Gautam <pranavgautam27@gmail.com>
2024-08-18 21:23:14 -0400

    Added ; where missing

--- JS/this2.js
+++ JS/this2.js
@@ -1,9 +1,9 @@
 function upDate(previewPic){
 	document.getElementById('image').innerHTML=previewPic.alt;
-	document.getElementById('image').style.backgroundImage="url("+previewPic.src+")"
+	document.getElementById('image').style.backgroundImage="url("+previewPic.src+")";
 }
 
 function unDo(){
 	document.getElementById('image').innerHTML="Hover over an image below to display here.";
-	document.getElementById('image').style.backgroundImage="url('')"
+	document.getElementById('image').style.backgroundImage="url('')";
 }
